dbc5c328daca02c2112a8f2b3ef95f6d47f0c7bf
Linhlq <lequanglinh698@gmail.com>
2022-05-09 16:36:20 +0700

    Layout-Practice: Create admin theme

--- app/code/Practice/AdvanceMultiselectConfig/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Practice_AdvanceMutilselectConfig', __DIR__);
--- app/code/Practice/Checkout/Model/Sales/Total/Quote/CustomTotal.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Practice\Checkout\Model\Sales\Total\Quote;
-
-class CustomTotal extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
-{
-
-}
